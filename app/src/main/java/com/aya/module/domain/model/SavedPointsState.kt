d2577696e24a7036b2f3eed24aab151e05b7d577
ekodoank89 <ayaekodoank@gmail.com>
2026-09-14 00:04:52 +0700

    Update SavedPointsState.kt

--- app/src/main/java/com/aya/module/domain/model/SavedPointsState.kt
+++ app/src/main/java/com/aya/module/domain/model/SavedPointsState.kt
@@ -1,9 +1,10 @@
 package com.aya.module.domain.model
 
-/** Kondisi titik A/B yang dipersistenkan ke disk */
+/** Kondisi titik A/B + favorit yang dipersistenkan ke disk */
 data class SavedPointsState(
     val isActiveA: Boolean = false,
     val pointA: LocationData? = null,
     val isActiveB: Boolean = false,
-    val pointB: LocationData? = null
+    val pointB: LocationData? = null,
+    val favorite: FavoritePoint? = null
 )
