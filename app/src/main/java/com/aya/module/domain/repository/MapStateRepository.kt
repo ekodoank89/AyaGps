e4992ea78cdb9222b236d590d9d63331aeb81234
ekodoank89 <ayaekodoank@gmail.com>
2026-09-13 21:35:31 +0700

    Update MapStateRepository.kt

--- app/src/main/java/com/aya/module/domain/repository/MapStateRepository.kt
+++ app/src/main/java/com/aya/module/domain/repository/MapStateRepository.kt
@@ -1,6 +1,8 @@
 package com.aya.module.domain.repository
 
+import com.aya.module.domain.model.SavedCameraState
 import com.aya.module.domain.model.SavedPanelLocks
+import com.aya.module.domain.model.SavedPanelOffsets
 import com.aya.module.domain.model.SavedPointsState
 
 interface MapStateRepository {
@@ -8,4 +10,8 @@ interface MapStateRepository {
     suspend fun load(): SavedPointsState
     suspend fun savePanelLocks(state: SavedPanelLocks)
     suspend fun loadPanelLocks(): SavedPanelLocks
+    suspend fun savePanelOffsets(state: SavedPanelOffsets)
+    suspend fun loadPanelOffsets(): SavedPanelOffsets
+    suspend fun saveCameraState(state: SavedCameraState)
+    suspend fun loadCameraState(): SavedCameraState?
 }
