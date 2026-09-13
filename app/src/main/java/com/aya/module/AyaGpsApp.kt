2f6b33eef05d148b0401de6306a8cd63aee3ad7f
ekodoank89 <ayaekodoank@gmail.com>
2026-09-13 20:05:26 +0700

    Update AyaGpsApp.kt

--- app/src/main/java/com/aya/module/AyaGpsApp.kt
+++ app/src/main/java/com/aya/module/AyaGpsApp.kt
@@ -4,7 +4,9 @@ import android.app.Application
 import android.content.Context
 import com.aya.module.data.location.LocationDataSource
 import com.aya.module.data.repository.LocationRepositoryImpl
+import com.aya.module.data.repository.MapStateRepositoryImpl
 import com.aya.module.domain.repository.LocationRepository
+import com.aya.module.domain.repository.MapStateRepository
 import com.aya.module.domain.usecase.GetCurrentLocationUseCase
 import com.aya.module.domain.usecase.GetLocationUpdatesUseCase
 
@@ -23,6 +25,9 @@ class AppContainer(appContext: Context) {
 
     private val locationRepository: LocationRepository =
         LocationRepositoryImpl(LocationDataSource(appContext))
+
+    val mapStateRepository: MapStateRepository =
+        MapStateRepositoryImpl(appContext)
 
     val getLocationUpdates = GetLocationUpdatesUseCase(locationRepository)
     val getCurrentLocation = GetCurrentLocationUseCase(locationRepository)
