968bfc2bbec68d4df14a7dcdd5d830368998c771
ekodoank89 <ayaekodoank@gmail.com>
2026-09-14 04:13:24 +0700

    Delete xposed-api-stub directory

--- xposed-api-stub/src/main/java/io/github/libxposed/api/XposedInterface.java
+++
@@ -1,9 +0,0 @@
-package io.github.libxposed.api;
-
-/** STUB kompilasi — implementasi asli disediakan framework LSPosed saat runtime. */
-public interface XposedInterface {
-
-    void log(String text);
-
-    void log(String prefix, Throwable throwable);
-}
