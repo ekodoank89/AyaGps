eb42288819d2aa785b3a59d7754840e6c97b6628
ekodoank89 <ayaekodoank@gmail.com>
2026-09-13 15:18:04 +0700

    Update MapContract.kt

--- app/src/main/java/com/aya/module/presentation/map/MapContract.kt
+++ app/src/main/java/com/aya/module/presentation/map/MapContract.kt
@@ -1,12 +1,20 @@
 package com.aya.module.presentation.map
 
+import com.aya.module.domain.model.LocationData
+
 /** ===== UI STATE ===== */
 data class MapUiState(
-    val hasPermission: Boolean = false
+    val hasPermission: Boolean = false,
+    val isTrackingA: Boolean = false,
+    val isTrackingB: Boolean = false,
+    val trackA: List<LocationData> = emptyList(),
+    val trackB: List<LocationData> = emptyList()
 )
 
 /** ===== INTENT ===== */
 sealed interface MapIntent {
     data object PermissionGranted : MapIntent
     data object PermissionDenied : MapIntent
+    data object ToggleTrackingA : MapIntent
+    data object ToggleTrackingB : MapIntent
 }
