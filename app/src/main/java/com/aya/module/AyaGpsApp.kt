3b98da4501dd4d48bb7d8f7407cf1ee9a09fbba0
ekodoank89 <ayaekodoank@gmail.com>
2026-09-13 13:42:52 +0700

    Create AyaGpsApp.kt

---
+++ app/src/main/java/com/aya/module/AyaGpsApp.kt
@@ -0,0 +1,29 @@
+package com.aya.module
+
+import android.app.Application
+import android.content.Context
+import com.aya.module.data.location.LocationDataSource
+import com.aya.module.data.repository.LocationRepositoryImpl
+import com.aya.module.domain.repository.LocationRepository
+import com.aya.module.domain.usecase.GetCurrentLocationUseCase
+import com.aya.module.domain.usecase.GetLocationUpdatesUseCase
+
+class AyaGpsApp : Application() {
+
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        container = AppContainer(this)
+    }
+}
+
+/** Dependency Injection manual (Clean Architecture) */
+class AppContainer(appContext: Context) {
+
+    private val locationRepository: LocationRepository =
+        LocationRepositoryImpl(LocationDataSource(appContext))
+
+    val getLocationUpdates = GetLocationUpdatesUseCase(locationRepository)
+    val getCurrentLocation = GetCurrentLocationUseCase(locationRepository)
+}
