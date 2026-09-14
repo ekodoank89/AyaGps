ca6645345c1cdeae31e670ea0eaa6bce48fdd11b
ekodoank89 <ayaekodoank@gmail.com>
2026-09-14 15:25:25 +0700

    Update MapScreen.kt

--- app/src/main/java/com/aya/module/presentation/map/MapScreen.kt
+++ app/src/main/java/com/aya/module/presentation/map/MapScreen.kt
@@ -3,6 +3,7 @@ package com.aya.module.presentation.map
 import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.PowerManager
